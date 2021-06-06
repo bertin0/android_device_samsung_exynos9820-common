7437af440fe2d32f3ffc9286559540f41694d147
Albert Geantă <albertgeanta@gmail.com>
2021-06-06 14:45:21 +0000

    Revert "Fix fod (fingerprint) permissions path for dot"
    This reverts commit 9be95efec0e955d010ab278f488433b0d7f9b467.

--- platform/permissions.mk
+++ platform/permissions.mk
@@ -65,5 +65,5 @@ PRODUCT_COPY_FILES += \
 
 ifeq ($(TARGET_HAVE_FOD),true)
 PRODUCT_COPY_FILES += \
-    vendor/dot/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
+    vendor/lineage/config/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/vendor.lineage.biometrics.fingerprint.inscreen.xml
 endif
